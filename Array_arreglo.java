7e4c27ec7ffe1fa7708e7567888db3b7c342f792
GiancarloMayen <47362498+GiancarloMayen@users.noreply.github.com>
2020-10-11 13:00:30 -0700

    Update Array_arreglo.java

--- Array_arreglo.java
+++ Array_arreglo.java
@@ -5,17 +5,18 @@ public class Array_arreglo {
 
     public static void main(String[] args) {
         
-        int[][]numeros=new int [10][4];
+        int[][]numeros=new int [5][4];
      
-        for(int relleno=0;relleno<10;relleno++){
+        for(int relleno=0;relleno<5;relleno++){
             for(int notas=0;notas<4;notas++){
             numeros[relleno][notas]=(int)(Math.random()*100);
             }
           }
       
-      for(int conteo=0;conteo<10;conteo ++){
+      for(int conteo=0;conteo<5;conteo ++){
       for(int con=0;con<4;con++){
           System.out.println("numero "+(conteo+1)+"="+numeros[conteo][con]);
+          System.out.println("todo terminado");
       }}
 
     } 
